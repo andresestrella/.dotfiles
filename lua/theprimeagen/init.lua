7ea3c77a3005bf5ef3fdaeef53a64afef9fbc1f7
Andrés Estrella <andresmauricioestrella@gmail.com>
2023-03-10 01:13:36 -0400

    added git git-signs also did this and that and something like that

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -1,6 +1,5 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
-require("theprimeagen.autocmd")
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
 
@@ -30,3 +29,44 @@ autocmd({"BufWritePre"}, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- Persistent Folds
+local save_fold = augroup("Persistent Folds", { clear = true })
+autocmd("BufWinLeave", {
+  pattern = "*.*",
+  callback = function()
+    vim.cmd.mkview()
+  end,
+  group = save_fold,
+})
+autocmd("BufWinEnter", {
+  pattern = "*.*",
+  callback = function()
+    vim.cmd.loadview({ mods = { emsg_silent = true } })
+  end,
+  group = save_fold,
+})
+-- Persistent Cursor
+autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- set up folding based on treesitter
+
+local opt = vim.opt
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+--open all fold on startup
+-- autocmd("BufReadPost", {
+--   pattern = "*.*",
+--   callback = function()
+--     vim.cmd("normal! zR")
+--   end,
+-- })
--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,18 +1,23 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+-- set timeoutlen to 200 ms
+vim.opt.timeoutlen = 200
 
+--tabs
+vim.opt.showtabline = 4 -- always show tabs
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
+vim.opt.softtabstop = 4 -- change number of space characters inserted for indentation
+vim.opt.shiftwidth = 4 -- change the number of space characters inserted for indentation
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.smarttab = true -- makes tabbing smarter will realize you have 4 vs 4
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.undofile = true
-
+vim.opt.backup = false -- creates a backup file
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
@@ -25,4 +30,5 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.title = true -- set the title of window to the value of the titlestring

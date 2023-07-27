e006f62c847a99eef1f3ce9aa3710292e47631ef
Andrés Estrella <andresmauricioestrella@gmail.com>
2023-07-27 00:17:17 -0400

    comment plugin

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 
 local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
 
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -22,6 +22,7 @@ require('telescope').setup{
 vim.keymap.set('n', '<leader>pt', ":Telescope<CR>")
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '?', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
